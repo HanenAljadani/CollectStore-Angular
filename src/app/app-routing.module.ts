9cf23242ac81e64d0cb98a006cd90e341b6f6cff
HanenAljadani <haneenabdullah66@hotmail.com>
2024-06-13 18:00:55 +0300

    fix the routing , and view the product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path:"home", component: HomeComponent},
   {path:"about", component: AboutComponent},
   {path:"phone", component: PhoneComponent},
-  {path:"phonedetails", component: PhoneDetailsComponent},
+  {path:"phonedetails/:id", component: PhoneDetailsComponent},
   {path:"products", component: ProductsComponent}
 ];
 
--- src/app/phone-details/phone-details.component.ts
+++ src/app/phone-details/phone-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-phone-details',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './phone-details.component.css'
 })
 export class PhoneDetailsComponent {
+  phones=[
+    {id:1, title:"iPhone",name:"iphone 15 pro max", price:3543, img:"/assets/images/i15Pro.jpeg"},
+    {id:2, title:"iPhone",name:"iPhone 14", price:2943, img:"/assets/images/i14.png"},
+    {id:3, title:"iPhone",name:"iPhone 13 pro max", price:2543, img:"/assets/images/i13.png"},
+    {id:4, title:"iPhone",name:"iPhone 13 pro", price:2000, img:"/assets/images/i13pro.webp"}
+   ]
+ 
+  id:any;
+  items:any;
+  constructor(private route:ActivatedRoute){}
+
+  ngOnInit(){
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.items = this.phones.find(e=>e.id == this.id)
+     
+
+  }
 
 }
